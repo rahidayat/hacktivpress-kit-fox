d7f44c6e3ad62ac6daa6f34b8c13ba88cdf6ef0b
rahid <hidayat.85@gmail.com>
2017-10-12 14:30:56 +0700

    get all dan get one artikel

--- server/controllers/articlesController.js
+++ server/controllers/articlesController.js
@@ -11,6 +11,22 @@ var create = (req, res) => {
   .catch(err => res.status(500).send({msg: 'gagal buat artikel baru'}))
 }
 
+var getAllArticles = (req, res) => {
+  Article.find({})
+  .populate('author', 'username')
+  .then(articles => res.send(articles))
+  .catch(err => res.status(500).send({msg: 'error proses'}))
+}
+
+var getOne = (req, res) => {
+  Article.findById(req.params.id)
+  .populate('author', 'username')
+  .then(article => res.send(article))
+  .catch(err => res.status(500).send({msg: 'error proses'}))
+}
+
 module.exports = {
-  create
+  create,
+  getAllArticles,
+  getOne
 }
--- server/routes/articlesRouter.js
+++ server/routes/articlesRouter.js
@@ -4,6 +4,8 @@ const articlesController = require('../controllers/articlesController')
 const authHelper = require('../helpers/authHelper')
 
 router.post('/', authHelper.checkLogin, authHelper.checkAuth, articlesController.create)
+router.get('/', articlesController.getAllArticles)
+router.get('/:id', articlesController.getOne)
 
 
 module.exports = router
