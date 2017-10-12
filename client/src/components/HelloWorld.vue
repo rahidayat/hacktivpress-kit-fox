ed5147a5cef54ce155b7d97c1b70c3f523be90cc
rahid <hidayat.85@gmail.com>
2017-10-12 15:43:46 +0700

    register di frontend

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,28 +1,27 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <!-- <Login/> -->
+          </div>
+          <div class="col-md-6">
+            <Register/>
+          </div>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
+import Login from '@/components/Login'
+import Register from '@/components/Register'
 export default {
   name: 'HelloWorld',
+  components: {
+    Register,
+    Login
+  },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
---
+++ client/src/components/Register.vue
@@ -0,0 +1,42 @@
+<template lang="html">
+  <div class="jumbotron">
+    <form class="form-horizontal" @submit.prevent="createUser(formRegister)">
+      <fieldset>
+        <legend>Sign up</legend>
+        <label class="control-label" for="focusedInput">Username</label>
+          <input class="form-control" placeholder="username" type="text" v-model="formRegister.username">
+        <label class="control-label" for="focusedInput">Password</label>
+          <input class="form-control" placeholder="password" type="password" v-model="formRegister.password"> <br>
+        <button class="btn btn-info" type="submit">Register</button>
+      </fieldset>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      formRegister: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    createUser () {
+      this.$http.post('/signup', this.formRegister)
+      .then(userData => {
+        console.log('user data register: ', userData)
+        this.$router.push('login')
+      })
+      .catch(err => {
+        console.log('ini error register: ', err.response.data.msg)
+      })
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
