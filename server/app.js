28794bf8ef4e4294536b2b9befc33121dd4421e0
rahid <hidayat.85@gmail.com>
2017-10-12 13:15:25 +0700

    set up app.js database connection

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/hacktivepress-kit', (err) => {
+  err ? console.log('Can\'t connect to database') : console.log('Database connected')
+});
+
+app.use(cors())
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log('I am listening port 3000');
+})
